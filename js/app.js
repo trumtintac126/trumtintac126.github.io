1fb37b1c38e19b5a6bebbf66978de82f1dc04759
trumtintac126 <nguyenmanhtri1261996@gmail.com>
2018-12-17 16:06:13 +0700

    modified style.css (edit full height in slide show,add work <section>,add button (back_to_top),handle event of button back-to-top in app.js)

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//--------slide--------//
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("service");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-opacity-off";
+}
+//-------back-to-top-----//
+var btn = $('#button_to_top');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+//-------Message--------//
+$('#sendMessage').hide();
